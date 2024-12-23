03fcb63c4facb4622cc230cef2456f2dec57f0c9
OU9098 <onyiugbaja58@gmail.com>
2024-12-23 23:19:22 +0000

    updates

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
-print("Welcome to the food odering Chatbot")
-name= input("What is your name")
+print ("Welcome to the food odering chatbot")
+name= input("What is your name?")
+age= input ("Hello "+name+ ",How old are you?")
